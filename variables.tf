1f2739352cb0e3a2fa517d356605f225e2d87844
keertika-20 <keertika.kulakarni@gmail.com>
2026-03-13 19:17:24 +0530

    Add type definitions for Azure variables

--- variables.tf
+++ variables.tf
@@ -1,11 +1,15 @@
 variable "subscription_id" {
   description = "Azure Subscription ID"
+  type        = string
 }
 
 variable "tenant_id" {
   description = "Azure Tenant ID"
+  type        = string
 }
 
 variable "client_id" {
   description = "Azure Client ID"
+  type        = string
 }
+
