9884bea2439d15722afb033636b5da588cec8509
keertika-20 <keertika.kulakarni@gmail.com>
2026-03-13 20:12:43 +0530

    Add Azure resources for network security and IP

---
+++ security.tf
@@ -0,0 +1,29 @@
+resource "azurerm_network_security_group" "demo_nsg" {
+  name                = "demo-nsg"
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_public_ip" "demo_pip" {
+  name                = "demo-pip"
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface_security_group_association" "demo_nic_nsg" {
+  network_interface_id      = azurerm_network_interface.demo_nic.id
+  network_security_group_id = azurerm_network_security_group.demo_nsg.id
+}
