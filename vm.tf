4ab4b606e1025b600999360a5fe9f9cf1e4a5450
keertika-20 <keertika.kulakarni@gmail.com>
2026-03-13 20:13:23 +0530

    Add Azure Linux VM resource configuration

---
+++ vm.tf
@@ -0,0 +1,28 @@
+resource "azurerm_linux_virtual_machine" "demo_vm" {
+  name                = "demo-vm"
+  resource_group_name = azurerm_resource_group.demo.name
+  location            = azurerm_resource_group.demo.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.demo_nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20.04-LTS"
+    version   = "latest"
+  }
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+}
