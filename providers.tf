8cf5fbd9b04c9d374f966eae5719a7d286d724d4
keertika-20 <keertika.kulakarni@gmail.com>
2026-03-13 18:30:48 +0530

    Add OIDC configuration to Azure provider

--- providers.tf
+++ providers.tf
@@ -9,6 +9,9 @@ terraform {
 
 provider "azurerm" {
   features {}
+
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
+  use_oidc        = true
+  client_id       = var.client_id
 }
