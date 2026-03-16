b7fb9268f2fdac718f46bffd78f3b748f25b9d60
keertika-20 <keertika.kulakarni@gmail.com>
2026-03-16 12:46:56 +0530

    Remove azurerm provider configuration
    Removed Terraform provider configuration for azurerm.

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.116.0"   # pin to a specific version
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "demo" {
   name     = "rg-terraform-demo"
   location = "westus2"
