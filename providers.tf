c15de914adcb6e4328c364b1f31fdc709c1f1a12
keertika-20 <keertika.kulakarni@gmail.com>
2026-03-16 14:18:20 +0530

    Change azurerm provider version constraint

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.116.0"   # pin to a specific version
+      version = "~> 3.115"
     }
   }
 }
